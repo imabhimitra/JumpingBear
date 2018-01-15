d12b38fb6b3d946b0396e0077b347263f6f52dd0
Abhi Mitra <apon555@gmail.com>
2018-01-15 16:07:05 +0530

    Please import these repository to unity and run.

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour {
+
+	public GameObject bear;
+	public GameObject lifeLine;
+
+	float bearPos =0f;
+	float lifeLinePos=0f;
+
+	public Text overText;
+	string over="Ohh Droped!!!";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bearPos = bear.transform.position.y;
+		lifeLinePos = lifeLine.transform.position.x;
+
+		if (bearPos < lifeLinePos) {
+
+			overText.text = over.ToString ();
+
+		}
+		
+	}
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	public GameObject bear;
+
+	float bearPos =0f;
+
+	public Text score;
+	int x=0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		bearPos = bear.transform.position.y;
+
+		if (x < bearPos) {
+			x++;
+		}
+//		int i = 0;
+//		for(i=5000;x>i;x++)
+//
+		score.text = x.ToString ();
+	}
+}
